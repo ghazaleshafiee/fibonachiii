74596ae90b3932b0d366ed28c34675efc8311320
ghazaleshafiee <130673766+ghazaleshafiee@users.noreply.github.com>
2023-06-09 22:40:36 +0330

    Add files via upload

---
+++ mabani fibonachii/mabani fibonachii/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mabani_fibonachii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(Fibonachi(i));
+            }
+            Console.ReadLine();
+            }
+        static int Fibonachi(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+    }
+}
